8621bdd63d576bf815f8d25d57bf4690cc9959d0
doppyn <jakub.smagur@gmail.com>
2021-09-22 12:42:23 +0200

    xd

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bbshop',
+    version='',
+    packages=['order', 'order.migrations', 'bbshop', 'product', 'product.migrations'],
+    url='',
+    license='',
+    author='doppyn',
+    author_email='',
+    description=''
+)
